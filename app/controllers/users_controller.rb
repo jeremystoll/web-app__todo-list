ae17d2773ff6a8e2b9cf10baaa98e395368aaadb
Jeremy Stoll <j8r8my@gmail.com>
2016-02-24 11:46:52 -0600

    added feature Create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,32 @@
 # This controller is for all the CRUD operations related to a User.
+MyApp.get "/" do
+
+erb :"home_page"
+end
+
+MyApp.get "/create_user" do
+
+erb :"users/create_user"
+end
+
+MyApp.post "/process_create_user" do
+
+  @user_new = User.new
+  
+  @user_new.email = params[:user_email]
+  @user_new.password = params[:user_password]
+  @user_new.save
+
+
+erb :"users/success_create_user"
+end
+
+MyApp.get "/update_user" do
+
+erb :"users/update_user"
+end
+
+MyApp.get "/delete_user" do
+
+erb :"users/delete_user"
+end
